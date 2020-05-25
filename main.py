a3347b1e2fc31fb0cc1f35acc6bf6097ee47a99e
Adam Venroy <16310@burnside.school.nz>
2020-05-26 09:24:14 +1200

    Created Template Pages and CSS, as well as main.py to run template page

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from database import *
 
 app.secret_key = 'supersecretkey'
 
 @app.route("/", methods=['GET'])
 @app.route("/home", methods=['GET'])
+def home():
+    return render_template('home.html')
+
+#If anything other than home is reached, it will be redirected to home page
+@app.errorhandler(404)
+def error(e):
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
